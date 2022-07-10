101c4da0a8d861f60e45420338d65f4c98b7e05e
unknown <abilsabili50@gmail.com>
2022-07-11 01:19:47 +0700

    membuat database wrapper dan menambahkan fitur detail mhs

---
+++ app/core/Database.php
@@ -0,0 +1,72 @@
+<?php
+
+class Database
+{
+    private $dbhost = DB_HOST;
+    private $dbuser = DB_USER;
+    private $dbpass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh; // database handler
+    private $statement;
+
+    public function __construct()
+    {
+        // data source name
+        $dsn = "mysql:host=" . $this->dbhost . ";dbname=" . $this->dbname;
+
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        try {
+            $this->dbh = new PDO($dsn, $this->dbuser, $this->dbpass, $options);
+        } catch (PDOException $err) {
+            die($err->getMessage());
+        }
+    }
+
+    public function query($query)
+    {
+        $this->statement = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->statement->bindValue($param, $value, $type);
+    }
+
+    public function execute()
+    {
+        $this->statement->execute();
+    }
+
+    public function resultSet()
+    {
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function single()
+    {
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_ASSOC);
+    }
+}
--- app/init.php
+++ app/init.php
@@ -2,4 +2,7 @@
 
 require_once(__DIR__ . "/core/App.php");
 require_once(__DIR__ . "/core/Controller.php");
-require_once(__DIR__ . "/core/Constants.php");
+require_once(__DIR__ . "/core/Database.php");
+
+
+require_once(__DIR__ . "/config/config.php");
